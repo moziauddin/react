7270bd96396992cb7c6944f526b95e686dc54345
Mo <moziauddin@gmail.com>
2020-09-21 14:41:58 +1000

    Added a directory struction project

---
+++ 03-multi-file/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import MenuItem from "./menuItem"
+
+function App() {
+    return (
+        <React.Fragment>
+            <h1>Hello World</h1>
+            <MenuItem />
+        </React.Fragment>   
+    )
+}
+
+export default App;
---
+++ 03-multi-file/menuItem.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function MenuItem() {
+    return (
+        <React.Fragment>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </React.Fragment>
+    )
+}
+
+export default MenuItem
