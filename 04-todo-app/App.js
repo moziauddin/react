7e3c6bb0054ec76e22e9c66a9e5847ed2c6808c9
Mo <moziauddin@gmail.com>
2020-09-21 14:57:06 +1000

    Added a simple to do app

---
+++ 04-todo-app/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" name="laundry"></input>
+            <p>Do Laundry</p>
+            <input type="checkbox" name="dishes"></input> 
+            <p>Do Dishes</p>
+            <input type="checkbox" name="vaccum"></input> 
+            <p>Do Vaccum</p>
+            <input type="checkbox" name="iron"></input> 
+            <p>Do Ironing</p>
+        </div>
+    )
+}
+
+export default App
